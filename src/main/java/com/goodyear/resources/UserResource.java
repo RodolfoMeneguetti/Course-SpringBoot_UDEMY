17fa02a5087aa61b04bdc2698ffc4ba76d18da63
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-02-23 16:37:58 -0300

    Service layer, component registration

--- src/main/java/com/goodyear/resources/UserResource.java
+++ src/main/java/com/goodyear/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.goodyear.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goodyear.entities.User;
+import com.goodyear.services.UserService;
 
 // para definir uma classe que sera a controlladora é informado a seguinte annotation
 
@@ -13,11 +18,19 @@ import com.goodyear.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	// criar um endpoint
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "12345"); 
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
