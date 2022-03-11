56854b89c5bc5f2cf57e92c5e3f685cc4fd931bc
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-03-11 14:46:34 -0300

    Order ,Instante, ISO 8601 - PART 2

--- src/main/java/com/goodyear/config/TestConfig.java
+++ src/main/java/com/goodyear/config/TestConfig.java
@@ -1,5 +1,6 @@
-package com.goodyear.config;
+ package com.goodyear.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.goodyear.entities.Order;
 import com.goodyear.entities.User;
+import com.goodyear.repositories.OrderRepository;
 import com.goodyear.repositories.UserRepository;
 
 //anotação para informar que a classe sera de configuração! 
@@ -19,6 +22,9 @@ public class TestConfig implements CommandLineRunner {
 	// O Spring resolve a dependencia com baixo acoplamento !!!
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	/* Função autoimplementada que ao estartar o projeto é inserindo os dados 
 	 * no banco de dados utilizando os paramentros da função!
@@ -30,8 +36,13 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		
 		// é possivel salvar mais de uma função em sequencia! 
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 	}
 	
---
+++ src/main/java/com/goodyear/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.goodyear.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.goodyear.entities.Order;
+import com.goodyear.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository; 
+
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
