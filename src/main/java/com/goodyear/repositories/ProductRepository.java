27553566edecf0cb38b669be364dff9e09179b74
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-03-14 11:31:58 -0300

    Product

---
+++ src/main/java/com/goodyear/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.goodyear.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.goodyear.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
