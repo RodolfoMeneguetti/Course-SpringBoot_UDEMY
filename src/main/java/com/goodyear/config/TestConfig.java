38c83d8d122906ca4e7e1cce2fc7523bc8c5e462
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-03-15 11:16:29 -0300

    Payment, one-to-one association

--- src/main/java/com/goodyear/config/TestConfig.java
+++ src/main/java/com/goodyear/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.goodyear.entities.Category;
 import com.goodyear.entities.Order;
 import com.goodyear.entities.OrderItem;
+import com.goodyear.entities.Payment;
 import com.goodyear.entities.Product;
 import com.goodyear.entities.User;
 import com.goodyear.entities.enums.OrderStatus;
@@ -90,6 +91,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		// chama a aplicação dps salva o pacote!
+		o1.setPayment(pay1);
+		// Salvando pacote 
+		orderRepository.save(o1);
 	}
 	
 
