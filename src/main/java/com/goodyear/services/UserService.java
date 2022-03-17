1cd27fe5ffe51a92c41d096bf8494b9fd6f929ad
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-03-17 14:39:22 -0300

    User update

--- src/main/java/com/goodyear/services/UserService.java
+++ src/main/java/com/goodyear/services/UserService.java
@@ -3,40 +3,58 @@ package com.goodyear.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.goodyear.entities.User;
 import com.goodyear.repositories.UserRepository;
+import com.goodyear.services.exceptions.DatabaseException;
+import com.goodyear.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
-	@Autowired
-	private UserRepository repository; 
 
-	public List<User> findAll(){
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
 	}
-	
+
 	public User update(Long id, User obj) {
-		// getOne = Prepara o Objeto para pesquisa sem necessariamente realizar uma pesquisa como o findById
+		// getOne = Prepara o Objeto para pesquisa sem necessariamente realizar uma
+		// pesquisa como o findById
+		try {
 		User entity = repository.getOne(id);
-		updateData(entity,obj);
+		updateData(entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
@@ -44,7 +62,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
-	
-	
+
 }
